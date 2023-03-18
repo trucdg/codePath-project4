56c99c844980fe7a26c43a167900637a3c9438c5
Truc Duong <trucdg@bu.edu>
2023-03-17 23:16:30 -0400

    add some initial styles to the MainDisplay component

--- caturday/src/component/MainDisplay.jsx
+++ caturday/src/component/MainDisplay.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "./MainDisplay.css";
 
 const MainDisplay = () => {
   return (
     <div className="main-display">
-      <h1>
-        The Caturday! <i class="fa-solid fa-shield-cat"></i>
-      </h1>
-      <p>Today is all about cats!</p>
-      <h2>龴ↀ◡ↀ龴</h2>
+      <header>
+        <h1>
+          The Caturday! <i class="fa-solid fa-shield-cat"></i>
+        </h1>
+        <p>Today is all about cats!</p>
+        <h2>龴ↀ◡ↀ龴</h2>
+      </header>
     </div>
   );
 };
