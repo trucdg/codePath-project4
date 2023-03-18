759393815ce7bb86948faccc227d15e36d15943a
Truc Duong <trucdg@bu.edu>
2023-03-17 23:55:34 -0400

    add hard coded information into the main display

--- caturday/src/component/MainDisplay.jsx
+++ caturday/src/component/MainDisplay.jsx
@@ -11,6 +11,21 @@ const MainDisplay = () => {
         <p>Today is all about cats!</p>
         <h2>龴ↀ◡ↀ龴</h2>
       </header>
+      <div>
+        <h2>Mimi - name</h2>
+        <div className="buttons-cont">
+          <button>alt name</button>
+          <button>life span</button>
+          <button>origin</button>
+          <button>metrics</button>
+        </div>
+        <div className="img-container">
+          <img
+            className="display-image"
+            src="https://cdn2.thecatapi.com/images/ap0.jpg"
+          />
+        </div>
+      </div>
       <button>
         <i className="fa-solid fa-shuffle"></i>Discover!
       </button>
