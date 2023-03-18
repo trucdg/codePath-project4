4278bf4af0145c035fca210e43e32360783bac3b
Truc Duong <trucdg@bu.edu>
2023-03-18 00:00:32 -0400

    add state variable to store cat information

--- caturday/src/App.jsx
+++ caturday/src/App.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import MainDisplay from "./component/MainDisplay";
 
 function App() {
+  const [cat, setCat] = useState({
+    name: "",
+    metric: "",
+    origin: "",
+    life_span: "",
+  });
+
   return (
     <div className="App">
       <MainDisplay />
