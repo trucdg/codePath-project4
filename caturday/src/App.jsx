f312ccce385ddf88b6be06dab20cbed408d97bbc
Truc Duong <trucdg@bu.edu>
2023-03-17 23:03:25 -0400

    add font awesome script and cat icon

--- caturday/src/App.jsx
+++ caturday/src/App.jsx
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>The Caturday!</h1>
+      <h1>
+        The Caturday! <i class="fa-solid fa-shield-cat"></i>
+      </h1>
       <p>Today is all about cats!</p>
     </div>
   );
